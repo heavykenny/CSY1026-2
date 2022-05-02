fb24215a0881f315f89d2cb4ec5e4cd6fada6bc0
Kehinde Olawuwo <olawuwokenny@gmail.com>
2022-05-02 19:57:41 +0100

    Adding queries to create tables and drop tables

---
+++ create_5.sql
@@ -0,0 +1,59 @@
+CREATE TABLE categories
+(
+    categories_id NUMBER(5)
+);
+
+CREATE TABLE tournaments
+(
+    tournaments_id NUMBER(5)
+);
+
+CREATE TABLE prizes
+(
+    prizes_id NUMBER(5)
+);
+
+CREATE TABLE sponsors
+(
+    sponsors_id NUMBER(5)
+);
+
+CREATE TABLE tournament_participants
+(
+    tournament_participants_id NUMBER(5)
+);
+
+CREATE TABLE game_participants
+(
+    game_participants_id NUMBER(5)
+);
+
+CREATE TABLE games
+(
+    games_id NUMBER(5)
+);
+
+CREATE TABLE rounds
+(
+    rounds_id NUMBER(5)
+);
+
+CREATE TABLE statistics
+(
+    statistics_id NUMBER(5)
+);
+
+CREATE TABLE teams
+(
+    teams_id NUMBER(5)
+);
+
+CREATE TABLE players
+(
+    players_id NUMBER(5)
+);
+
+CREATE TABLE team_players
+(
+    team_players_id NUMBER(5)
+);
---
+++ drop_5.sql
@@ -0,0 +1,14 @@
+DROP TABLE categories;
+DROP TABLE tournaments;
+DROP TABLE prizes;
+DROP TABLE sponsors;
+DROP TABLE tournament_participants;
+DROP TABLE game_participants;
+DROP TABLE games;
+DROP TABLE rounds;
+DROP TABLE statistics;
+DROP TABLE teams;
+DROP TABLE players;
+DROP TABLE team_players;
+
+PURGE RECYCLEBIN;
