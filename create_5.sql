9e201d8134f835dea61c9f28b9e87bb97bd90c42
Kehinde Olawuwo <olawuwokenny@gmail.com>
2022-05-14 12:11:24 +0100

    Fixing missing comma and lints

--- create_5.sql
+++ create_5.sql
@@ -2,7 +2,7 @@
 
 CREATE TABLE players
 (
-    player_id       NUMBER(8),
+    player_id        NUMBER(8),
     player_firstname VARCHAR2(15),
     player_lastname  VARCHAR2(15),
     contact_no       CHAR(11),
@@ -11,7 +11,7 @@ CREATE TABLE players
 
 CREATE TABLE teams
 (
-    team_id  NUMBER(8),
+    team_id   NUMBER(8),
     team_name VARCHAR2(15)
 );
 
@@ -24,22 +24,22 @@ CREATE TABLE team_players
 
 CREATE TABLE categories
 (
-    category_id  NUMBER(8),
-    game_name     VARCHAR2(18),
-    game_genre    VARCHAR2(10)
+    category_id NUMBER(8),
+    game_name   VARCHAR2(18),
+    game_genre  VARCHAR2(10)
 );
 
 CREATE TABLE tournaments
 (
     tournament_id NUMBER(8),
-    category_id    NUMBER(8) NOT NULL,
-    start_date     DATE DEFAULT SYSDATE,
-    end_date       DATE DEFAULT (SYSDATE + 30)
+    category_id   NUMBER(8) NOT NULL,
+    start_date    DATE DEFAULT SYSDATE,
+    end_date      DATE DEFAULT (SYSDATE + 30)
 );
 
 CREATE TABLE sponsors
 (
-    sponsor_id  NUMBER(8),
+    sponsor_id   NUMBER(8),
     sponsor_name VARCHAR2(10)
 );
 
@@ -53,29 +53,29 @@ CREATE TABLE prizes
 
 CREATE TABLE tournament_participants
 (
-    tournament_participant_id NUMBER(8)
-    tournament_id NUMBER(8) NOT NULL,
-    player_id     NUMBER(8),
-    team_id       NUMBER(8)
+    tournament_participant_id NUMBER(8),
+    tournament_id             NUMBER (8) NOT NULL,
+    player_id                 NUMBER(8),
+    team_id                   NUMBER(8)
 );
 
 CREATE TABLE rounds
 (
-    round_id         NUMBER(8),
+    round_id          NUMBER(8),
     round_level       NUMBER(10),
     level_description VARCHAR2(15)
 );
 
 CREATE TABLE games
 (
-    game_id   NUMBER(8),
+    game_id    NUMBER(8),
     round_id   NUMBER(8),
     start_time DATE DEFAULT SYSDATE
 );
 
 CREATE TABLE statistics
 (
-    statistic_id NUMBER(8),
+    statistic_id  NUMBER(8),
     game_id       NUMBER(8) NOT NULL,
     game_duration NUMBER(11),
     game_kills    NUMBER(10) DEFAULT 0,
@@ -84,8 +84,8 @@ CREATE TABLE statistics
 
 CREATE TABLE game_participants
 (
-    game_participant_id NUMBER(8),
+    game_participant_id       NUMBER(8),
     tournament_participant_id NUMBER(8) NOT NULL,
-    game_id NUMBER(8) NOT NULL
+    game_id                   NUMBER(8) NOT NULL
 );
 
