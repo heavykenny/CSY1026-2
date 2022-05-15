ba690fe8667b5fed3b12b9eb92774efd5b5a81a6
mufarom <101185164+mufarom@users.noreply.github.com>
2022-05-15 11:24:00 +0100

    Queries To Show All Records From Each Table Completed

--- queries_5.sql
+++ queries_5.sql
@@ -3,3 +3,36 @@
 -- ALL RECORDS FROM EACH TABLE
 SELECT *
 FROM players;
+
+SELECT *
+FROM teams;
+
+SELECT *
+FROM team_players;
+
+SELECT *
+FROM categories;
+
+SELECT *
+FROM tournaments;
+
+SELECT *
+FROM sponsors;
+
+SELECT *
+FROM prizes;
+
+SELECT *
+FROM tournament_participants;
+
+SELECT *
+FROM rounds;
+
+SELECT *
+FROM games;
+
+SELECT *
+FROM statistics;
+
+SELECT *
+FROM game_participants;
